f18100f4e933d9054446e09e8410d51ab62d3361
Valentín Ceballos <108242764+vjceballosj@users.noreply.github.com>
2025-08-21 16:57:54 -0500

    spring initializr backend

---
+++ backend/src/main/java/com/system/payments/SystemPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.system.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemPaymentsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/system/payments/SystemPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
