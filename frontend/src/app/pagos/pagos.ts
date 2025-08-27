a5a0d126b9fefe7891f9a986a30b4923aa8077b4
Valentín Ceballos <108242764+vjceballosj@users.noreply.github.com>
2025-08-27 17:14:59 -0500

    Update pagos.html mat-paginator show tables

--- frontend/src/app/pagos/pagos.ts
+++ frontend/src/app/pagos/pagos.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-pagos',
@@ -11,9 +12,9 @@ import { MatSort } from '@angular/material/sort';
 })
 export class Pagos {
 
-  public pagos:any;
-  public dataSource:any;
-  public displayedColumns = ['id','fecha','cantidad','type','status','nombre'];
+  public pagos: any;
+  public dataSource: any;
+  public displayedColumns = ['id', 'fecha', 'cantidad', 'type', 'status', 'nombre'];
 
   /*
   - @ViewChild: decorador que permite acceder a un componente hijo del DOM
@@ -21,9 +22,20 @@ export class Pagos {
   @ViewChild(MatPaginator) panigator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private http:HttpClient){}
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.http.get("http://localhost:8080/pagos").subscribe({
+      next: data => {
+        this.pagos = data;
+        this.dataSource = new MatTableDataSource(this.pagos);
+        this.dataSource.paginator = this.panigator;
+        this.dataSource.sort = this.sort;
+      },
+      error: err => {
+        console.log(err);
+      }
+
+    })
   }
 }
